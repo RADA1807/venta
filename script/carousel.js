1304f528e5f5688e2c795da8dd941843d1f70485
RADA1807 <robert1807@hotmail.com>
2025-04-24 13:09:02 -0600

    listo

---
+++ script/carousel.js
@@ -0,0 +1,41 @@
+var swiper = new Swiper('.swiper-container', {
+    loop: true, // Carrusel infinito
+    slidesPerView: 3, // Muestra siempre 3 cards visibles
+    spaceBetween: 30, // Espacio entre ellas
+    centeredSlides: true, // Centra las 3 cards visibles
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+	slidesPerView: 1,
+	spaceBetween: 10,
+	// init: false,
+	pagination: {
+	  el: '.swiper-pagination',
+	  clickable: true,
+	},
+
+  
+	breakpoints: {
+	  620: {
+		slidesPerView: 1,
+		spaceBetween: 20,
+	  },
+	  680: {
+		slidesPerView: 2,
+		spaceBetween: 40,
+	  },
+	  920: {
+		slidesPerView: 3,
+		spaceBetween: 40,
+	  },
+	  1240: {
+		slidesPerView: 4,
+		spaceBetween: 50,
+	  },
+	} 
+    });
